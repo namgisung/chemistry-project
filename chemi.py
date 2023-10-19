42a48f90a821cc0904650eec5d50629368e9261c
namgisung <109130108+namgisung@users.noreply.github.com>
2023-10-20 02:33:42 +0900

    Update chemi.py

--- chemi.py
+++ chemi.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-#반응 전 A와 B의 양이 주어진 양적관계
-def reaction(a_mole_ratio, b_mole_ratio, c_mole_ratio):
+#반응 전 A믜 양이 주어진 첨가반응물 양적관계
+def reacting(a_mole_ratio, b_mole_ratio, c_mole_ratio):
   #A의 반응하기전 몰수 입력받기
   a_mole = float(input("A의 반응하기 전 양을 입력하세요:"))
-  b_mole = float(input("B의 반응하기 전 양을 입력하세요:"))
+  b_mole = 15
   cm = 0
   bing = []
   t = []
@@ -47,6 +47,9 @@ def reaction(a_mole_ratio, b_mole_ratio, c_mole_ratio):
     limiting_reactant = "B"
   else:
     limiting_reactant = "A and B"
+  
+
+  
   print("몰비: ", a_mole_ratio, b_mole_ratio, c_mole_ratio)
   print("한계반응물: ", limiting_reactant)
   print("반응 후 A의 양:", after_a_mole)
@@ -76,6 +79,59 @@ def total_whole_mole(a_mole_ratio,b_mole_ratio,c_mole_ratio):
   c_mole2 = c_mole_ratio * (total_moles_change / total_modulus_change)
   print("반응 후 C의 양:", c_mole2)
 
+#반응 전 A와 B의 양이 주어진 양적관계
+def reaction(a_mole_ratio, b_mole_ratio, c_mole_ratio):
+  #A의 반응하기전 몰수 입력받기
+  a_mole = float(input("A의 반응하기 전 양을 입력하세요:"))
+  b_mole = float(input("B의 반응하기 전 양을 입력하세요:"))
+  cm = 0
+  bing = []
+  t = []
+  total_calculate = 0
+  limiting_reactant = ""
+  after_a_mole = 0
+  after_b_mole = 0
+  i = 0
+
+  #한계반응물 찾고 반응후 몰수 구하기()
+  
+  if(a_mole / a_mole_ratio > b_mole / b_mole_ratio):
+    after_a_mole = a_mole - (b_mole / b_mole_ratio) * a_mole_ratio
+    after_b_mole = 0
+    cm = (b_mole_ratio / b_mole_ratio) * c_mole_ratio
+    total_calculate = cm + after_a_mole
+  elif(a_mole / a_mole_ratio < b_mole / b_mole_ratio):
+    after_b_mole = b_mole - (a_mole / a_mole_ratio) * b_mole_ratio
+    after_a_mole = 0
+    cm = (a_mole / a_mole_ratio) *  c_mole_ratio
+    total_calculate = cm + after_b_mole
+  else:
+    cm = (a_mole / a_mole_ratio) * c_mole_ratio
+    after_a_mole = 0
+    after_b_mole = 0
+    total_calculate = cm
+
+  if a_mole == 0:
+    total_calculate = cm + 1
+  elif b_mole == 0:
+    total_calculate = cm + 1
+      
+    
+
+  if after_a_mole == 0 and after_b_mole > 0:
+    limiting_reactant = "A"
+  elif after_b_mole == 0 and after_a_mole > 0:
+    limiting_reactant = "B"
+  else:
+    limiting_reactant = "A and B"
+  print("몰비: ", a_mole_ratio, b_mole_ratio, c_mole_ratio)
+  print("한계반응물: ", limiting_reactant)
+  print("반응 후 전체 양:", total_calculate)
+  print("반응 후 A의 양:", after_a_mole)
+  print("반응 후 B의 양:", after_b_mole)
+  print("반응 후 C의 양:", cm)
+
+
 #화학반응식 입력받기
 ce = input("화학반응식을 입력하세요(_A+_B->_C):")
 q = float(input("제시된 자료를 숫자로 입력하세요(반응 전 A와 B의 양(1), 반응 전과 후 전체 양(2))"))
@@ -90,6 +146,8 @@ b_mole_ratio = int(re2.split("B")[0])
 c_mole_ratio = int(pr.split("C")[0])
 
 if q == 1:
-  reaction(a_mole_ratio,b_mole_ratio,c_mole_ratio)
+  reacting(a_mole_ratio,b_mole_ratio,c_mole_ratio)
 elif q ==2:
   total_whole_mole(a_mole_ratio,b_mole_ratio,c_mole_ratio)
+elif q ==3:
+  reaction(a_mole_ratio, b_mole_ratio, c_mole_ratio)
