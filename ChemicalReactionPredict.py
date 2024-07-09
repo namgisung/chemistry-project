a51a2f8e8c828bbaca9ab32fb57324a92114b4b3
namgisung <109130108+namgisung@users.noreply.github.com>
2024-07-09 10:39:53 +0900

    ChemicalReactionPredict.py 업데이트

--- ChemicalReactionPredict.py
+++ ChemicalReactionPredict.py
@@ -9,11 +9,11 @@ def calculate_equilibrium_constant(reactant_concentrations, product_concentratio
     return equilibrium_constant
 
 def calculate_reaction_index(reactant_concentrations, product_concentrations, reactant_coefficients, product_coefficients):
-    reaction_index = 0.0
+    reaction_index = 1.0
     for i in range(len(product_concentrations)):
-        reaction_index += product_coefficients[i] * math.log(product_concentrations[i])
+        reaction_index *= product_concentrations[i] ** product_coefficients[i]
     for i in range(len(reactant_concentrations)):
-        reaction_index -= reactant_coefficients[i] * math.log(reactant_concentrations[i])
+        reaction_index /= reactant_concentrations[i] ** reactant_coefficients[i]
     return reaction_index
 
 def predict_chemical_reaction(equilibrium_constant, reaction_index):
@@ -27,26 +27,32 @@ def predict_chemical_reaction(equilibrium_constant, reaction_index):
 num_reactants = int(input("반응물의 물질 개수: "))
 num_products = int(input("생성물의 물질 개수: "))
 
+eq_reactant_concentrations = []
 reactant_concentrations = []
 reactant_coefficients = []
+eq_product_concentrations = []
 product_concentrations = []
 product_coefficients = []
 
 print("반응물의 평형 농도와 화학 빈응 계수를 입력:")
 for i in range(num_reactants):
-    concentration = float(input(f"반응물 {i +1}의 평형 농도: "))
+    eq_concentration = float(input(f"반응물 {i +1}의 평형 농도: "))
+    concentration = float(input(f"반응물 {i +1}의 현재 농도: "))
     coefficient = int(input(f"반응물 {i + 1}의 화학 반응 계수: "))
     reactant_concentrations.append(concentration)
     reactant_coefficients.append(coefficient)
+    eq_reactant_concentrations.append(eq_concentration)
 
 print("생성물의 평형 농도와 화학 반응 계수를 입력:")
 for i in range(num_products):
-    concentration = float(input(f"생성물 {i +1}의 평형 농도: "))
+    eq_concentration = float(input(f"생성물 {i +1}의 평형 농도: "))
+    concentration = float(input(f"생성물 {i +1}의 현재 농도: "))
     coefficient = int(input(f"생성물 {i +1}의 화학 반응 계수: "))
     product_concentrations.append(concentration)
     product_coefficients.append(coefficient)
+    eq_product_concentrations.append(eq_concentration)
 
-equilibrium_constant = calculate_equilibrium_constant(reactant_concentrations, product_concentrations, reactant_coefficients, product_coefficients)
+equilibrium_constant = calculate_equilibrium_constant(eq_reactant_concentrations, eq_product_concentrations, reactant_coefficients, product_coefficients)
 reaction_index = calculate_reaction_index(reactant_concentrations, product_concentrations, reactant_coefficients, product_coefficients)
 
 print("평형 상수:", equilibrium_constant)
